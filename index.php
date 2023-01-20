37991c054479b84ac1530bb9210aee689c3d9055
ines667 <mathis.colpin.29@gmail.com>
2023-01-20 16:17:21 +0100

    cequejeveux

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ include "script_php/script.php";
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style/index.css">
+        <script src="script_php/script.js"></script>
         <title>FACIL</title>
         <link rel="icon" href="favicon.webp" sizes="196x196">
 
@@ -52,33 +53,74 @@ include "script_php/script.php";
                         </div>
 
                         <!-- CV -->
-                        <h4>CV</h4>
+                        <h3>CV</h3> <H5 class="obligatoire">Obligatoire</H5> 
+                        
+
                         <input class = "file" type = "file" name="upload_file_cv" title=" " /> <br/>
-                        <input class = "button" type = "submit" name="submit_cv"/> <br/>
+                        <input id="bouton_cv" class = "button" type = "submit" name="submit_cv"/> <br/>
 
                         <div class="barre">
                         </div>
 
                         <!-- CNI ou autres -->
-                        <h4>CNI, si pas de carte d'identité veuillez mettre : passeport, titre de séjour et autorisation de travail</h4>
+                        <h3>Carte Nationale D'identité</h3> <H5 class="obligatoire" type="text">Obligatoire</H5>
+                        <h4>Si vous ne possédez pas de carte d'identité veuillez mettre : passeport, titre de séjour et autorisation de travail dans les champs correspondants</h4>
                         <input class = "file" type = "file" name="upload_file_cni" /> <br/>
                         <input class = "button" type = "submit" name="submit_cni"/> <br/>
 
                         <div class="barre">
                         </div>
+                        <!-- passeport -->
+                        <h3>Passeport : Uniquement si vous ne possédez pas de Carte D'identité</h3> <H5 class="obligatoire" type="text">Obligatoire si pas de CNI</H5>
+                        <input class = "file" type = "file" name="upload_file_passport" /> <br/>
+                        <input class = "button" type = "submit" name="submit_passport"/> <br/>
+                        <!-- titre_de_sejour -->
+                        <h3>Titre de séjour : Uniquement si vous ne possédez pas de Carte D'identité</h3> <H5 class="obligatoire" type="text">Obligatoire si pas de CNI</H5>
+                        <input class = "file" type = "file" name="upload_file_titre_de_sejour" /> <br/>
+                        <input class = "button" type = "submit" name="submit_titre_de_sejour"/> <br/>
+                        <!-- Autorisation de travail -->
+                        <h3>Autorisation de travail : Uniquement si vous ne possédez pas de Carte D'identité</h3> <H5 class="obligatoire" type="text">Obligatoire si pas de CNI</H5>
+                        <input class = "file" type = "file" name="upload_file_autorisation_de_travail" /> <br/>
+                        <input class = "button" type = "submit" name="submit_autorisation_de_travail"/> <br/>
+                        <div class="barre">
+                        </div>
+                        <!-- Carte Vitale -->
+                        <h3>Carte Vitale</h3> <H5 class="obligatoire" type="text">Obligatoire</H5>
+                        <input class = "file" type = "file" name="upload_file_vitale" /> <br/>
+                        <input class = "button" type = "submit" name="submit_vitale"/> <br/>
+                        <div class="barre">
+                        </div>
 
-                        <h4>Passeport</h4>
-                        <input class = "file" type = "file" name="upload_file_titre" /> <br/>
-                        <input class = "button" type = "submit" name="submit_titre"/> <br/>
+                        <!-- RIB -->
+                        <h3>RIB</h3> <H5 class="obligatoire" type="text">Obligatoire</H5>
+                        <input class = "file" type = "file" name="upload_file_rib" /> <br/>
+                        <input class = "button" type = "submit" name="submit_rib"/> <br/>
+                        <div class="barre">
+                        </div>
 
-                        <h4>Titre de séjour</h4>
-                        <input class = "file" type = "file" name="upload_file_test" /> <br/>
-                        <input class = "button" type = "submit" name="submit_test"/> <br/>
+                        <!-- carte_grise -->
+                        <h3>Carte Grise</h3> 
+                        <input class = "file" type = "file" name="upload_file_carte_grise" /> <br/>
+                        <input class = "button" type = "submit" name="submit_carte_grise"/> <br/>
 
-                        <h4>Autorisation de travail</h4>
-                        <input class = "file" type = "file" name="upload_file_test2" /> <br/>
-                        <input class = "button" type = "submit" name="submit_test2"/> <br/>
 
+                        <!-- veh_assu -->
+                        <h3>Assurance de Véhicule</h3> 
+                        <input class = "file" type = "file" name="upload_file_veh_assu" /> <br/>
+                        <input class = "button" type = "submit" name="submit_veh_assu"/> <br/>
+
+
+                        <!-- Permis -->
+                        <h3>Permis</h3> <H5 class="obligatoire" type="text">Obligatoire</H5>
+                        <input class = "file" type = "file" name="upload_file_permis" /> <br/>
+                        <input class = "button" type = "submit" name="submit_permis"/> <br/>
+                        <div class="barre">
+                        </div>
+
+                        <!-- justif_domi -->
+                        <h3>Justification de Domicile</h3> <H5 class="obligatoire" type="text">Obligatoire</H5>
+                        <input class = "file" type = "file" name="upload_file_justif_domi" /> <br/>
+                        <input class = "button" type = "submit" name="submit_justif_domi"/> <br/>
                         <div class="barre">
                         </div>
 
--- script_php/script.php
+++ script_php/script.php
@@ -4,7 +4,6 @@ session_start();
 
 // Se connecter au serveur
 $con = ftp_connect('80.11.12.120', 21);
-
 $login = ftp_login($con, 'RhFacil', 'Facil2023');
 
 
@@ -17,28 +16,17 @@ $login = ftp_login($con, 'RhFacil', 'Facil2023');
         if (isset($_POST["create_directory"])){
 
                 $collabname = $_POST['collab_name'];
-                
-
-        
                 $_SESSION['NomPrenom']=$_POST['collab_name'];
                 $dir = "collab/" .$_SESSION['NomPrenom'];
                 $dir2 = $dir . "/document";
-                // if (ftp_mkdir($con, $dir)){
-                //         echo "<p class='echo_connecter'>Bienvenue ".$collabname."</p>";
-                //         ftp_mkdir($con, $dir2);
-                // } else{
-                //         echo "Le dossier existe deja";
-                // };  
-                
+
                 $files = ftp_nlist($con, "collab");
                 if(in_array("collab/".$collabname, $files)) {
-                echo "<p class='echo_connecter_false'>Le dossier existe deja</p>";
-                } else {
+                echo "<p class='echo_connecter_false'>Le dossier existe deja</p>";} 
+                else {
                 echo "<p class='echo_connecter'>Bienvenue ".$collabname."</p>";
                 ftp_mkdir($con, $dir);
-                ftp_mkdir($con, $dir2);
-                };
-
+                ftp_mkdir($con, $dir2);};
         }
 
         $dir = "collab/" .$_SESSION['NomPrenom'];
@@ -48,24 +36,16 @@ $login = ftp_login($con, 'RhFacil', 'Facil2023');
         //bouton connect
         if (isset($_POST["connect_directory"])){
 
-
                 $collabname = $_POST['collab_name'];
-                
-
-        
                 $_SESSION['NomPrenom']=$_POST['collab_name'];
                 $dir = "collab/" .$_SESSION['NomPrenom'];
                 $dir2 = $dir . "/document";
 
-
                 $files = ftp_nlist($con, "collab");
                 if(in_array("collab/".$collabname, $files)) {
-                echo "<p class='echo_connecter'>Vous etes bien connecter à ".$collabname."</p>";
-                } else {
-                echo "<p class='echo_connecter_false'>Le dossier n'existe pas.</p>";
-                };
-
-
+                echo "<p class='echo_connecter'>Vous etes bien connecter à ".$collabname."</p>";} 
+                else {
+                echo "<p class='echo_connecter_false'>Le dossier n'existe pas.</p>";};
         }
         
 
@@ -73,71 +53,173 @@ $login = ftp_login($con, 'RhFacil', 'Facil2023');
         //CV
         if (isset($_POST["submit_cv"]))
         {
-
                 $fileName = $_FILES['upload_file_cv']['name'];
                 $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
-
-
                 $tmpname = $_FILES['upload_file_cv']['tmp_name'];
                 $uniqueName = md5(uniqid(rand(), true));
                 $fileName = "./upload/" . $uniqueName . $fileExt;
-                // $resultat = move_uploaded_file($tmpname, $fileName);
-
-
 
                 // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/CV_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi du CV.</p>";}
+                else{echo "Erreur";}
+        }
 
-                // if ($file = ftp_put($con, $dir2 . "/cni" .  $_COOKIE['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0))
-                if ($file = ftp_put($con, $dir2 . "/CV_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0))
-                {
-                
-                echo "<p class='echo_sucess'>Transfert réussi de la carte d'identité</p>";
-                }
-                else
-                {
-                echo "Erreur";
-                }
+        //IDcard
+        if (isset($_POST["submit_cni"]))
+        {
+                $fileName = $_FILES['upload_file_cni']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_cni']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
 
-                // Fermer la connexion
-                ftp_close($con);
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/CNI_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de la carte d'identité</p>";}
+                else{echo "Erreur";}
         }
 
 
 
-        //IDCARD
-        // if (isset($_POST["submit_"]))
-        // {
-        //     echo "ok";
+        //passport
+        if (isset($_POST["submit_passport"]))
+        {
+                $fileName = $_FILES['upload_file_passport']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_passport']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
 
-        //         $fileName = $_FILES['upload_file_cv']['name'];
-        //         $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
-
-
-        //         $tmpname = $_FILES['upload_file_cv']['tmp_name'];
-        //         $uniqueName = md5(uniqid(rand(), true));
-        //         $fileName = "./upload/" . $uniqueName . $fileExt;
-        //         // $resultat = move_uploaded_file($tmpname, $fileName);
-
-
-        //         // Se connecter au serveur
-        //         $con = ftp_connect('80.11.12.120', 21);
-
-        //         $login = ftp_login($con, 'RhFacil', 'Facil2023');
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Passeport_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre Passeport.</p>";}
+                else{echo "Erreur";}
+        }
 
 
+        //titre_de_sejour
+        if (isset($_POST["submit_titre_de_sejour"]))
+        {
+                $fileName = $_FILES['upload_file_titre_de_sejour']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_titre_de_sejour']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
 
-        //         // Uploader le fichier
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Titre_De_Sejour_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre titre de séjour.</p>";}
+                else{echo "Erreur";}
+        }
 
-        //         if ($file = ftp_put($con, $dir2 . "/cni" .  $collabname . $fileExt, $tmpname, FTP_BINARY, 0))
-        //         {
-        //         echo "Transfert réussi de la carte d'identité";
-        //         }
-        //         else
-        //         {
-        //         echo "Erreur";
-        //         }
+        //autorisation de travail
+        if (isset($_POST["submit_autorisation_de_travail"]))
+        {
+                $fileName = $_FILES['upload_file_autorisation_de_travail']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_autorisation_de_travail']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
 
-        //         // Fermer la connexion
-        //         ftp_close($con);
-        // }
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Autorisation_De_Travail_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre autorisation de travail.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //vital
+        if (isset($_POST["submit_vital"]))
+        {
+                $fileName = $_FILES['upload_file_vital']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_vital']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Carte_Vitale_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre Carte Vitale.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //rib
+        if (isset($_POST["submit_rib"]))
+        {
+                $fileName = $_FILES['upload_file_rib']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_rib']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/RIB_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre RIB.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //carte_grise
+        if (isset($_POST["submit_carte_grise"]))
+        {
+                $fileName = $_FILES['upload_file_carte_grise']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_carte_grise']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Carte_Grise_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre carte grise.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //veh_assu
+        if (isset($_POST["submit_veh_assu"]))
+        {
+                $fileName = $_FILES['upload_file_veh_assu']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_veh_assu']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Assurance_Véhicule_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre Assurance Véhicule.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //permis
+        if (isset($_POST["submit_permis"]))
+        {
+                $fileName = $_FILES['upload_file_permis']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_permis']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Permis_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre permis.</p>";}
+                else{echo "Erreur";}
+        }
+
+        //justif_domi
+        if (isset($_POST["submit_justif_domi"]))
+        {
+                $fileName = $_FILES['upload_file_justif_domi']['name'];
+                $fileExt = "." . strtolower(substr(strrchr($fileName, "."), 1));
+                $tmpname = $_FILES['upload_file_justif_domi']['tmp_name'];
+                $uniqueName = md5(uniqid(rand(), true));
+                $fileName = "./upload/" . $uniqueName . $fileExt;
+
+                // Uploader le fichier
+                if ($file = ftp_put($con, $dir2 . "/Justififcation_Domicile_" . $_SESSION['NomPrenom'] . $fileExt, $tmpname, FTP_BINARY, 0)){
+                echo "<p class='echo_sucess'>Transfert réussi de votre Justification de Domicile.</p>";}
+                else{echo "Erreur";}
+                session_destroy();
+        }        
+
+
+
+
 ?>
